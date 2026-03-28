64a3340ef56ee6763c574952368a57e8bbb444ec
Akash Dode <dgw.akashdode@gmail.com>
2026-03-27 20:10:20 -0400

    updated

--- Weather_project/Weather_project/settings.py
+++ Weather_project/Weather_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=n)m_wylbf3c_kwlck6+@35qq$!pf$uf1&2uwloa-awil=@-8q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'weather' / 'static',  # explicit path as fallback
 ]
